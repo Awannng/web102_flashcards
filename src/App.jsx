51bbec8bd61032bcbc8e983873d8de410a651b64
Jing Wang <jingwangw12@gmail.com>
2025-03-01 17:55:08 -0500

    funtional flashcards

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,47 @@
 import React, { useState } from "react";
+import vocab from "./vocabulary";
 
 const App = () => {
   const [count, setCount] = useState(0);
+  const [flip, setFlip] = useState(false);
+  const vocabs = vocab[count];
 
   const nextCard = () => {
-    if (count < 9) {
-      setCount(count + 1);
-    } else {
-      setCount(0);
-    }
+    setCount((prevCount) => (prevCount + 1) % vocab.length);
+    setFlip(false);
   };
   const backCard = () => {
     if (count > 0) {
       setCount(count - 1);
     }
+    setFlip(false);
   };
 
+  const handelFlip = () => {
+    setFlip(!flip);
+  };
   return (
     <>
       <div className="container">
         <div>
           <h1>Explore the Japense language!!!</h1>
           <h2>Try to memorize the vocabularies that is used in daily life!</h2>
-          <h2> {count + 1} / 10</h2>
+          <h2>
+            {count + 1} / {vocab.length}
+          </h2>
         </div>
 
-        <div className="card">
-          
+        {/* Display the flashcards */}
+        <div className="card" onClick={handelFlip}>
+          <div className={flip ? "hide" : ""}>
+            <p>{vocabs.japanese}</p>
+            <p>{vocabs.romanji}</p>
+          </div>
+
+          <p>{flip && vocabs.english}</p>
         </div>
 
+        {/* Buttons for going back and forth */}
         <div className="container_button">
           <button onClick={backCard}>Back</button>
           <button onClick={nextCard}>Next</button>
---
+++ src/vocabulary.js
@@ -0,0 +1,34 @@
+const vocab = [
+  { japanese: "おはよう", romanji: "Ohayou", english: "Good Morning" },
+  { japanese: "こんばんは", romanji: "Gonbanwa", english: "Good evening" },
+  { japanese: "こんにちは", romanji: "Konnichiwa", english: "Hello" },
+  { japanese: "ありがとう", romanji: "Arigatou", english: "Thank you" },
+  { japanese: "ごめなさい", romanji: "Gomenasai", english: "I'm sorry" },
+  {
+    japanese: "おやすみ",
+    romanji: "Oyasumi",
+    english: "Good night",
+  },
+  {
+    japanese: "いただきます",
+    romanji: "Itadakimasu",
+    english: "I'm going to start the meal",
+  },
+  {
+    japanese: "ごちそうさまでした",
+    romanji: "Gochisousama deshita",
+    english: "Thank you for the meal",
+  },
+  {
+    japanese: "そうですか",
+    romanji: "Sou desu ka",
+    english: "Is that so?",
+  },
+  {
+    japanese: "だいじょうぶです",
+    romanji: "Daijoubu desu",
+    english: "It's ok",
+  },
+];
+
+export default vocab;
